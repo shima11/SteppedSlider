ca8f5d14b5a74c39c4389920e39cf191a2a13e55
shima11 <shima.jin@icloud.com>
2024-11-30 21:25:21 +0900

    cleanup

--- Sources/SteppedSlider/SteppedSlider.swift
+++ Sources/SteppedSlider/SteppedSlider.swift
@@ -154,51 +154,6 @@ public struct SteppedSlider: View {
   }
 }
 
-// https://uvolchyk.medium.com/scrolling-pickers-in-swiftui-de4a9c653fb6
-
-/// A structure that defines a snapping behavior for scroll targets, conforming to `ScrollTargetBehavior`.
-struct SnapScrollTargetBehavior: ScrollTargetBehavior {
-  /// The step value to which the scroll target should snap.
-  let step: Double
-
-  /// Computes the closest multiple of `b` to the given value `a`.
-  /// - Parameters:
-  ///   - a: The value to snap.
-  ///   - b: The step to which `a` should snap.
-  /// - Returns: The closest multiple of `b` to `a`.
-  private func closestMultiple(
-    a: Double,
-    b: Double
-  ) -> Double {
-    let lowerMultiple = floor((a / b)) * b
-    let upperMultiple = floor(lowerMultiple + b)
-
-    return if abs(a - lowerMultiple) <= abs(a - upperMultiple) {
-      lowerMultiple
-    } else {
-      upperMultiple
-    }
-  }
-
-  func updateTarget(
-    _ target: inout ScrollTarget,
-    context: TargetContext
-  ) {
-    let x1 = target.rect.origin.x
-    let x2 = closestMultiple(a: x1, b: step)
-
-    target.rect.origin.x = x2
-  }
-}
-
-extension ScrollTargetBehavior where Self == SnapScrollTargetBehavior {
-  /// Creates a `SnapScrollTargetBehavior` with the specified step.
-  /// - Parameter step: The step value to which the scroll target should snap.
-  /// - Returns: A `SnapScrollTargetBehavior` instance with the given step value.
-  static func snap(step: Double) -> SnapScrollTargetBehavior { .init(step: step) }
-}
-
-
 #if DEBUG
 
 // https://youtu.be/5S08AZ8cYek
