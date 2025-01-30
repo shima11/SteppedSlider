e30eb26c6b65ccc8e06090c84b01b78921079759
shima11 <shima.jin@icloud.com>
2025-01-30 12:22:58 +0900

    update

--- Sources/SteppedSlider/SteppedSlider.swift
+++ Sources/SteppedSlider/SteppedSlider.swift
@@ -4,7 +4,7 @@
 import Foundation
 import SwiftUI
 
-public struct SteppedSlider: View {
+public struct SteppedSlider<Anchor: View, Segment: View, SegmentOverlay: View>: View {
 
   @Binding private var value: CGFloat
 
@@ -24,26 +24,31 @@ public struct SteppedSlider: View {
 
   private var onEditing: @MainActor () -> Void
 
+  let anchorView: () -> Anchor
+  let segmentView: (_ index: Int, _ maximumIndex: Int) -> Segment
+  let segmentOverlayView: (_ index: Int, _ maximumIndex: Int) -> SegmentOverlay
+
   public init(
     value: Binding<CGFloat>,
     range: ClosedRange<CGFloat>,
     steps: CGFloat,
     itemWidth: CGFloat = 10,
     spacing: CGFloat = 0,
+    @ViewBuilder anchorView: @MainActor @escaping () -> Anchor,
+    @ViewBuilder segmentView: @MainActor @escaping (Int, Int) -> Segment,
+    @ViewBuilder segmentOverlayView: @MainActor @escaping (Int, Int) -> SegmentOverlay,
     onEditing: @MainActor @escaping () -> Void
   ) {
-
     self._value = value
     self.range = range
     self.steps = steps
     self.itemWidth = itemWidth
     self.spacing = spacing
+    self.anchorView = anchorView
+    self.segmentView = segmentView
+    self.segmentOverlayView = segmentOverlayView
     self.onEditing = onEditing
     self.currentIndex = .init(SteppedSlider.calculateCurrentIndex(value: value.wrappedValue, steps: steps, range: range))
-  }
-
-  private func shouldBold(index: Int) -> Bool {
-    index == 0 || index == maximumIndex || (index % 5 == 0)
   }
 
   public var body: some View {
@@ -51,32 +56,20 @@ public struct SteppedSlider: View {
       ScrollView(.horizontal, showsIndicators: false) {
         LazyHStack(spacing: spacing) {
           ForEach(0...maximumIndex, id: \.self) { index in
-            let isBold = shouldBold(index: index)
-            ZStack {
-              Rectangle()
-                .foregroundStyle(Color.primary)
-                .frame(width: isBold ? 2 : 1, height: isBold ? 24 : 12)
-            }
-            .frame(width: itemWidth)
-            .overlay(
-              ZStack {
-                if shouldBold(index: index) {
-                  Text(String(format: "%g", CGFloat(index) * steps + range.lowerBound))
-                    .font(.caption2)
-                    .fixedSize()
-                    .offset(y: -20)
-                }
-              }
-            )
-            .padding(.top, 12)
-            .id(index)
-            .scrollTransition(
-              axis: .horizontal,
-              transition: { content, phase in
-                content
-                  .opacity(phase.isIdentity ? 1.0 : 0.3)
-              }
-            )
+            segmentView(index, maximumIndex)
+              .frame(width: itemWidth)
+              .overlay(
+                segmentOverlayView(index, maximumIndex)
+              )
+              .id(index)
+            // TODO: init時に外から変更させる処理を任せる
+//              .scrollTransition(
+//                axis: .horizontal,
+//                transition: { content, phase in
+//                  content
+//                    .opacity(phase.isIdentity ? 1.0 : 0.3)
+//                }
+//              )
           }
         }
         .scrollTargetLayout()
@@ -85,18 +78,8 @@ public struct SteppedSlider: View {
       .scrollPosition(id: $scrollIndex, anchor: .center)
       .scrollTargetBehavior(.snap(step: spacing + itemWidth))
       .overlay(
-        Rectangle()
-          .frame(width: 2, height: 24)
-          .foregroundColor(.red)
-          .padding(.top, 12)
+        anchorView()
       )
-      .mask({
-        HStack(spacing: 0) {
-          LinearGradient(colors: [.black.opacity(0), .black], startPoint: .leading, endPoint: .trailing).frame(width: 24)
-          Color.black
-          LinearGradient(colors: [.black, .black.opacity(0)], startPoint: .leading, endPoint: .trailing).frame(width: 24)
-        }
-      })
       .onGeometryChange(for: CGSize.self, of: \.size, action: {
         contentSize = $0
       })
@@ -136,15 +119,57 @@ public struct SteppedSlider: View {
 
 #Preview {
 
-  @Previewable @State var value: CGFloat = 5.5
+  @Previewable @State var value: CGFloat = 5.8
 
   let range: ClosedRange<CGFloat> = 1...20
+  let steps = 0.1
+  var shouldBold: (Int, Int) -> Bool = { index, maximumIndex -> Bool in
+    index == 0 || index == maximumIndex || (index % 5 == 0)
+  }
 
   VStack {
 
-    SteppedSlider(value: $value, range: range, steps: 0.1, onEditing: {})
-      .frame(height: 60)
-      .padding()
+    SteppedSlider(
+      value: $value,
+      range: range,
+      steps: steps,
+      anchorView: {
+        Rectangle()
+          .frame(width: 2, height: 24)
+          .foregroundColor(.red)
+          .padding(.top, 12)
+      },
+      segmentView: { index, maximumIndex in
+        let isBold = shouldBold(index, maximumIndex)
+        ZStack {
+          Rectangle()
+            .foregroundStyle(Color.primary)
+            .frame(width: isBold ? 2 : 1, height: isBold ? 24 : 12)
+            .padding(.top, 12)
+        }
+      },
+      segmentOverlayView: { index, maximumIndex in
+        ZStack {
+          if shouldBold(index, maximumIndex) {
+            Text(String(format: "%g", CGFloat(index) * steps + range.lowerBound))
+              .font(.caption2)
+              .fixedSize()
+              .offset(y: -20)
+              .padding(.top, 12)
+          }
+        }
+      },
+      onEditing: {}
+    )
+    .mask({
+      HStack(spacing: 0) {
+        LinearGradient(colors: [.black.opacity(0), .black], startPoint: .leading, endPoint: .trailing).frame(width: 24)
+        Color.black
+        LinearGradient(colors: [.black, .black.opacity(0)], startPoint: .leading, endPoint: .trailing).frame(width: 24)
+      }
+    })
+    .frame(height: 60)
+    .padding()
 
     Spacer(minLength: 24).fixedSize()
 
